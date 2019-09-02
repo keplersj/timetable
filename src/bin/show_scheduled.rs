f12685ab20a0ad36d13feb145506e34a19c4ffed
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-01 22:43:19 -0600

    Move diesel binary

--- src/bin/show_scheduled.rs
+++ src/bin/show_scheduled.rs
@@ -1,29 +1,29 @@
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
-mod models;
-mod schema;
 
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
 
 use diesel::prelude::*;
-use schema::*;
-use models::*;
+use timetable::models::*;
+use timetable::schema;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
 fn main() {
     use schema::scheduled_requests::dsl::*;
 
     let connection = establish_connection();
-    let results = scheduled_requests.filter(executed.eq(false))
+    let results = scheduled_requests
+        .filter(executed.eq(false))
         .limit(5)
         .load::<ScheduledRequest>(&connection)
         .expect("Error loading requests");
