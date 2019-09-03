0551513c16d01b84f11b106d99af56c58fa9c144
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-03 15:19:34 -0600

    Implement stub methods for routes detailed in README

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -6,9 +6,35 @@ fn index() -> impl Responder {
         .finish()
 }
 
+fn create_scheduled_request() -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
+fn delete_scheduled_request() -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
+fn get_scheduled_request_status() -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
+fn modify_scheduled_request() -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
 fn main() {
     let address = "127.0.0.1:8088";
-    let create_app = || App::new().route("/", web::get().to(index));
+    let create_app = || {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/schedule", web::post().to(create_scheduled_request))
+            .route("/schedule/{id}", web::delete().to(delete_scheduled_request))
+            .route(
+                "/schedule/{id}",
+                web::get().to(get_scheduled_request_status),
+            )
+            .route("/schedule/{id}", web::put().to(modify_scheduled_request))
+    };
 
     HttpServer::new(create_app)
         .bind(address)
