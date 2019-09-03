7c56dd4aed025b801f3a80b9af6027fcdb01655e
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-03 15:33:52 -0600

    Add logging middleware

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use env_logger;
 
 fn index() -> impl Responder {
     HttpResponse::TemporaryRedirect()
@@ -23,9 +24,13 @@ fn modify_scheduled_request() -> impl Responder {
 }
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let address = "127.0.0.1:8088";
     let create_app = || {
         App::new()
+            .wrap(Logger::default())
             .route("/", web::get().to(index))
             .route("/schedule", web::post().to(create_scheduled_request))
             .route("/schedule/{id}", web::delete().to(delete_scheduled_request))
@@ -36,6 +41,7 @@ fn main() {
             .route("/schedule/{id}", web::put().to(modify_scheduled_request))
     };
 
+    println!("Starting server on {}", address);
     HttpServer::new(create_app)
         .bind(address)
         .unwrap()
