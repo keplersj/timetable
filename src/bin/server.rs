8f391c6bc60976edc984d454ca8ac90b75a78d5f
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-03 15:05:01 -0600

    Configure root path to redirect to github

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,17 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::TemporaryRedirect()
+        .header("Location", "https://github.com/keplersj/timetable")
+        .finish()
 }
 
 fn main() {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8088")
+    let address = "127.0.0.1:8088";
+    let create_app = || App::new().route("/", web::get().to(index));
+
+    HttpServer::new(create_app)
+        .bind(address)
         .unwrap()
         .run()
         .unwrap();
