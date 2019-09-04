20ffa4ec91f6f14289b5fb876e8eee0a1d08b0ae
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-03 21:54:13 -0600

    Create response type in Rust

--- src/web/data.rs
+++ src/web/data.rs
@@ -1,5 +1,6 @@
 #![allow(non_snake_case)]
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Deserialize, Debug)]
 pub struct ScheduleExecutionPayload {
@@ -15,4 +16,14 @@ pub struct ScheduledExecutionStatus {
     pub targetWebhook: String,
     pub responseWebhook: String,
     pub status: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct ExecutionResponse {
+    id: String,
+    scheduledTimestamp: String,
+    targetWebhook: String,
+    status: u8,
+    headers: HashMap<String, String>,
+    body: String,
 }
